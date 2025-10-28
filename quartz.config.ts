7613b9efe79b014dcf5dea8cefecf9aff9cce4d7
xzhangBST <xzhang@benefitsscience.com>
2025-10-28 11:57:31 -0400

    logo

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "CSA-AP",
+    pageTitle: "AP Chinese Classroom",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
