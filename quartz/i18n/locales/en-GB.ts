0999d4ca1039c7a9ea6c5ab6a7ad435ba20008d3
xzhangBST <xzhang@benefitsscience.com>
2025-11-04 10:46:51 -0500

    Quartz sync: Nov 4, 2025, 10:46 AM

--- quartz/i18n/locales/en-GB.ts
+++ quartz/i18n/locales/en-GB.ts
@@ -39,7 +39,7 @@ export default {
       createdWith: "Created with",
     },
     graph: {
-      title: "Graph View",
+      title: "Knowledge Map",
     },
     recentNotes: {
       title: "Recent Notes",
