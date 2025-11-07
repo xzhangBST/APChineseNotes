9ee8730717d42fc10efc0a47f86bdeec71bb031d
xzhangBST <xzhang@benefitsscience.com>
2025-11-07 09:47:39 -0500

    Quartz sync: Nov 7, 2025, 9:47 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "{{GA_ID}}",
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
