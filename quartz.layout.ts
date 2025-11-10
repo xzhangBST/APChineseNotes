c603a65752ed85446eb40c43915ef86c9babd4c8
xzhangBST <xzhang@benefitsscience.com>
2025-11-10 10:38:58 -0500

    Quartz sync: Nov 10, 2025, 10:38 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/xzhangBST/APChineseNotes",
+      "Contact Me": "xzboston2016@gmail.com",
     },
   }),
 }
